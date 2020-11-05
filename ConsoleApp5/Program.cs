dcc04ca67357743f386a788bae3754670c5d06b6
Lera112555 <73837980+Lera112555@users.noreply.github.com>
2020-11-05 15:29:32 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Kit", new DateTime(2005, 8, 7));
+
+            Console.WriteLine($"Коту по имени {cat.Name} уже {cat.GetAge()} лет");
+
+            cat.HungryStatus = 150;
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
